a0bd17c9d3d5bd4b5164d7d4b873248f6a797390
eeee0717 <chentao020717Work@outlook.com>
2024-05-28 15:44:57 +0800

    chore

--- server/api/getEpisode.post.ts
+++ server/api/getEpisode.post.ts
@@ -1,20 +1,36 @@
 import type { Episode } from '~/types'
 
-export default defineEventHandler(async (event) => {
-  const episode: Episode = await readBody(event)
+async function fetchEpisode(episode: Episode) {
   const url = `https://api.xiaoyuzhoufm.com/v1/episode/get?eid=${episode.eid}`
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'x-jike-refresh-token': episode.appToken.freshToken,
-      'x-jike-device-id': episode.appToken.deviceId,
-      'x-jike-access-token': episode.appToken.accessToken,
-    },
-  }).then(res => res.json())
-  episode.title = response.data.title
-  episode.description = response.data.description
-  episode.datePublished = response.data.pubDate
-  episode.mediaUrl = response.data.enclosure.url
-  episode.picUrl = response.data.image.picUrl
-  return episode
+  const headers = {
+    'x-jike-refresh-token': episode.appToken.freshToken,
+    'x-jike-device-id': episode.appToken.deviceId,
+    'x-jike-access-token': episode.appToken.accessToken,
+  }
+  const response = await fetch(url, { method: 'GET', headers })
+  if (!response.ok) {
+    throw new Error(`Network response was not ok: ${response.statusText}`)
+  }
+  const data = await response.json()
+  if (!data || typeof data !== 'object') {
+    throw new Error('Response data is not valid')
+  }
+  return data
+}
+
+export default defineEventHandler(async (event) => {
+  try {
+    const episode: Episode = await readBody(event)
+    const data = await fetchEpisode(episode)
+    episode.title = data.title
+    episode.description = data.description
+    episode.datePublished = data.pubDate
+    episode.mediaUrl = data.enclosure.url
+    episode.picUrl = data.image.picUrl
+    return episode
+  }
+  catch (error) {
+    console.error(error)
+    throw error
+  }
 })
