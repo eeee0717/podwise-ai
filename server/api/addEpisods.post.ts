8bf3e74c8652be74f1177b16bee515e6bcd6cc3c
eeee0717 <chentao020717Work@outlook.com>
2024-05-31 19:37:09 +0800

    chore

--- server/api/addEpisods.post.ts
+++ server/api/addEpisods.post.ts
@@ -2,24 +2,30 @@ import { serverSupabaseClient } from '#supabase/server'
 import type { Episode } from '~/types'
 
 export default defineEventHandler(async (event) => {
-  const supabase = await serverSupabaseClient(event)
-  const { pid, episods } = await readBody(event)
-  const insertData: Episode[] = episods.map((episode: Episode) => {
-    return {
-      pid,
-      eid: episode.eid,
-      title: episode.title,
-      datePublished: episode.datePublished,
-      duration: episode.duration,
-      description: episode.description,
-      mediaUrl: episode.mediaUrl,
-      picUrl: episode.picUrl,
-    }
-  },
-  )
-  // eslint-disable-next-line ts/ban-ts-comment
-  // @ts-expect-error
-  const { data: responseData, error } = await supabase.from('episods').insert(insertData).select('*')
-  console.log(responseData, error)
-  return { data: responseData, msg: 'podcast added' }
+  try {
+    const supabase = await serverSupabaseClient(event)
+    const { pid, episods } = await readBody(event)
+    const insertData: Episode[] = episods.map((episode: Episode) => {
+      return {
+        pid,
+        eid: episode.eid,
+        title: episode.title,
+        datePublished: episode.datePublished,
+        duration: episode.duration,
+        description: episode.description,
+        mediaUrl: episode.mediaUrl,
+        picUrl: episode.picUrl,
+      }
+    })
+    // eslint-disable-next-line ts/ban-ts-comment
+    // @ts-expect-error
+    const { data: responseData, error } = await supabase.from('episods').insert(insertData).select('*')
+    if (error)
+      throw error
+    return { data: responseData, msg: 'podcast added' }
+  }
+  catch (error) {
+    console.error(error)
+    return { msg: 'An error occurred while adding the podcast' }
+  }
 })
