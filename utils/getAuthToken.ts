92941334b43a3a5e0ecb04f2b864ee8c55735db9
eeee0717 <chentao020717Work@outlook.com>
2024-06-05 20:30:51 +0800

    refactor get auth token

--- utils/getAuthToken.ts
+++ utils/getAuthToken.ts
@@ -1,24 +1,36 @@
 import type { authToken } from '~/types'
 
+// 如果deviceId是固定值，考虑定义为常量
+const DEFAULT_DEVICE_ID = '69F65C43-9B15-4621-AAA2-83F894DC0B51'
+
 const accessToken = useLocalStorage('accessToken', '')
-const deviceId = useLocalStorage('deviceId', '')
+const deviceId = useLocalStorage('deviceId', DEFAULT_DEVICE_ID)
 const freshToken = useLocalStorage('freshToken', '')
+
 export async function getAuthToken() {
-  const response = await $fetch('/api/getToken', {
-    method: 'POST',
-    body: {
-      accessToken,
-    },
-  }).catch((err) => {
-    console.error(err)
-    return null
-  })
-  if (response === null)
-    return null
-  const authToken = response as authToken
-  accessToken.value = authToken['x-jike-access-token']
-  freshToken.value = authToken['x-jike-refresh-token']
-  deviceId.value = '69F65C43-9B15-4621-AAA2-83F894DC0B51'
+  try {
+    const response = await $fetch('/api/getToken', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        accessToken: accessToken.value,
+      }),
+    })
+    if (!response) {
+      throw new Error('No response from getToken API')
+    }
+    const { 'x-jike-access-token': access, 'x-jike-refresh-token': refresh } = response as authToken
+    if (!access || !refresh) {
+      throw new Error('Invalid response structure for tokens')
+    }
+    accessToken.value = access
+    freshToken.value = refresh
+    deviceId.value = DEFAULT_DEVICE_ID // 可以在这里设置，如果设备ID是动态的，应该在其他地方设置
+  }
+  catch (err) {
+    console.error('Error fetching auth token:', err)
+    throw err // 或根据你的错误处理策略返回一个特定的错误对象
+  }
 }
 
 export function getAppToken() {
