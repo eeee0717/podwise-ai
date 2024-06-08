203d76d7297cfddb3a4621c053cb31b12675a205
eeee0717 <chentao020717Work@outlook.com>
2024-06-08 14:37:24 +0800

    refactor: change menubar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts">
+const links = ref([
+  { label: 'Home', icon: 'i-carbon-home', to: '/' },
+  { label: 'Episods', icon: 'i-carbon-media-library', to: '/episods' },
+  { label: 'Podcasts', icon: 'i-material-symbols-podcasts-rounded', to: '/podcasts' },
+])
+</script>
+
 <template>
   <div class="w-screen h-screen of-hidden" grid="~ rows-[max-content_1fr]">
     <TheNav />
     <div grid="~ cols-[min-content_1fr] gap-8">
-      <MenuBar />
+      <UVerticalNavigation
+        :links="links" class="justify-center m-l-13 lt-md:m-l-26px lt-md:w-8 "
+      />
       <div class="overflow-scroll h-90vh">
         <slot />
       </div>
